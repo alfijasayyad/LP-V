6c0ef936f516e15c14be65dd6cc24be8f761c109
Alfija Sayyad <70462781+alfijasayyad@users.noreply.github.com>
2023-05-27 07:43:38 +0530

    Add files via upload

---
+++ RMI/ImplExample.java
@@ -0,0 +1,9 @@
+// Implementing the remote interface 
+public class ImplExample implements Hello {  
+   
+    // Implementing the interface method 
+    public void printMsg() {  
+       System.out.println("Hii All !!"); 
+       System.out.println("This Is An Example RMI Program");  
+    }  
+ }    
